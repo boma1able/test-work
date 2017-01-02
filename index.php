348be6136f25ac4f9e5bd3f0c959a83fd84d4d89
boma <bomaooo@gmail.com>
2017-01-02 19:14:02 +0200

    html+css website ready

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>umm...design?</title>
+  
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet"> </head>
+
+<body>
+
+<!-- header -->
+   <div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <a href="index.php">
+        <div class="logo"> <img class="img-responsive" src="img/logo.png" alt="logo-image"> </div>
+      </a>
+    </div>
+  </div>
+</div>
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+    </div>
+    <div class="collapse navbar-collapse" id="main-menu">
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">home<span class="sr-only">(current)</span></a></li>
+        
+        <li><a href="about.php">about</a></li>  
+        
+        <li><a href="contacts.php">Contacts</a></li>
+      </ul>
+      <div class="login-panel"><a href="">Login</a>/<a href="">Register</a></div>
+    </div>
+  </div>
+</nav>
+
+  <!-- content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="post">
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <div class="date">01.01.17</div>
+          <div class="author">Admin</div> <img class="img-responsive" src="img/2.jpg" alt="image">
+          <div class="post-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum ullam possimus vitae molestias incidunt qui, corporis aliquam. Ipsa, atque voluptatum corporis magni, molestiae consequuntur voluptas praesentium, veniam quibusdam facere sunt. </p>
+          </div>
+          <button class="submit">read more</button>
+        </div>
+        <div class="post">
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <div class="date">01.01.17</div>
+          <div class="author">Admin</div> <img class="img-responsive" src="img/3.jpg" alt="image">
+          <div class="post-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum ullam possimus vitae molestias incidunt qui, corporis aliquam. Ipsa, atque voluptatum corporis magni, molestiae consequuntur voluptas praesentium, veniam quibusdam facere sunt.</p>
+          </div>
+          <button class="submit">read more</button>
+        </div>
+      </div>
+      <div class="col-md-4">
+        
+        <!-- sidebar -->
+        
+  <div class="sidebar">
+    <div class="search widget">
+      <form class="navbar-form">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search"> </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    </div>
+    <div class="popular-post-widget">
+     <h3>Popular posts</h3>
+      <ul>
+        <li><a href="">Admin on popular post № 1</a></li>
+        <li><a href="">Admin on popular post № 2</a></li>
+        <li><a href="">Admin on popular post № 3</a></li>
+      </ul>
+    </div>
+    <div class="recent-comment-widget">
+      <h3>Recent comments</h3>
+      <ul>
+        <li><a href="">Admin on popular post № 1</a></li>
+        <li><a href="">Admin on popular post № 2</a></li>
+        <li><a href="">Admin on popular post № 3</a></li>
+      </ul>
+    </div>
+  </div>
+        
+      </div>
+    </div>
+  </div>
+  
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="footer">
+          <div class="copy">&copy; 2017 umm...design?</div>
+          <div class="owner">Powered by boma</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
